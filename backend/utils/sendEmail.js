906c8852737b45427ede8de81995cc5d037a0356
Smit Mathukiya <smitmathukiya1695@gmail.com>
2026-03-25 08:02:33 +0530

    make correcteion config file for sending the email

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -1,9 +1,10 @@
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com", // required in production,
-    port: 465,  // required in production
-    secure: true, // required in production
+    host: "smtp.gmail.com", // required in production
+    port: 587,          // ✅ use 587 instead of 465 (required in production)
+    secure: false,      // ✅ must be false for 587 (required in production)
+    family: 4,          // ✅ force IPv4 (fix ENETUNREACH error) (required in production)
     //service: "gmail", // works in development
     auth: {
         user: process.env.EMAIL_USER,

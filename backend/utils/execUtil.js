14f1dd14671d092cbe7aa857ca9e9299f01453ae
Smit Mathukiya <smitmathukiya1695@gmail.com>
2026-06-08 10:43:57 +0530

    Create branch fastapi-migration

--- backend/utils/execUtil.js
+++ backend/utils/execUtil.js
@@ -3,7 +3,7 @@ const ytDlp = require("yt-dlp-exec");
 const downloadYoutubeVideo = async (url, outputPath) => {
     try {
         await ytDlp(url, {
-            format: "bestvideo[height<=720]/bestvideo", // video only
+            format: "bestvideo+bestaudio/best", // video only
             output: outputPath, // .mp4 or .webm
             jsRuntimes: "node"
         });
